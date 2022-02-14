b796f3b287cfa13e01154b8fcd1f2ed9ac22133f
Lucas Raposeiras <raposeiras.lucas@gmail.com>
2022-02-14 01:30:11 -0300

    Columns layout for Payment confirmation view

--- app/src/lib/components/payments/CheckoutItemCost/Breakdown/CheckoutItemCostBreakdown.tsx
+++ app/src/lib/components/payments/CheckoutItemCost/Breakdown/CheckoutItemCostBreakdown.tsx
@@ -1,9 +1,9 @@
-import { Box, Typography, Grid, Avatar, Divider, Stack } from "@mui/material";
+import { Divider, Stack } from "@mui/material";
 import React from "react";
-import { Number } from "../../../shared/Number/Number";
 import { CheckoutItemCostTotal } from "../Total/CheckoutItemCostTotal";
 import { CheckoutItem } from "../../../../domain/product/product.interfaces";
 import { useCheckoutItemsCostTotal } from "../../../../hooks/useCheckoutItemCostTotal";
+import { CheckoutItemList } from "../List/CheckoutItemList";
 
 export interface CheckoutItemCostBreakdownProps {
   checkoutItems: CheckoutItem[];
@@ -16,62 +16,13 @@ export const CheckoutItemCostBreakdown: React.FC<
 
   return (
     <Stack sx={{ display: "flex", flex: 1, py: 5 }}>
-      {checkoutItems.map(
-        ({ lotID, name, units, unitPrice, imageSrc, imageBackground }) => (
-          <Grid
-            key={lotID}
-            container
-            item
-            direction="column"
-            sx={{
-              display: "flex"
-            }}
-          >
-            <Box sx={{ flex: 1, display: "flex" }}>
-              <Avatar
-                alt={name}
-                src={imageSrc}
-                variant="square"
-                sx={{
-                  background: (theme) =>
-                    imageBackground || theme.palette.grey["300"],
-                  width: 80,
-                  height: 80,
-                  flex: "0 0 auto"
-                }}
-              />
-
-              <Box
-                sx={{
-                  marginLeft: 2,
-                  marginTop: 0.5,
-                  display: "flex",
-                  flexDirection: "column",
-                  flex: 1,
-                  justifyContent: "space-between"
-                }}
-              >
-                <Typography sx={{ fontWeight: "500" }}>{name}</Typography>
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between"
-                  }}
-                >
-                  <Typography sx={{ marginTop: 0.5 }}>{units}</Typography>
-                  <Typography sx={{ marginTop: 0.5 }}>
-                    <Number suffix=" USD">{unitPrice}</Number>
-                  </Typography>
-                </Box>
-              </Box>
-            </Box>
-
-            <Divider sx={{ my: 3 }} />
-          </Grid>
-        )
-      )}
-      <CheckoutItemCostTotal total={total} taxes={0} fees={fees} />
+      <CheckoutItemList
+        checkoutItems={checkoutItems}
+        withSeparators
+        showPrices
+      />
+      <Divider sx={{ mt: 3.75, mb: 1.5 }} />
+      <CheckoutItemCostTotal withDetails total={total} taxes={0} fees={fees} />
     </Stack>
   );
 };
