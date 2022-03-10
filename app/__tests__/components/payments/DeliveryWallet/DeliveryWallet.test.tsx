66fbc3a685d218ee1f2e22ff1d12ddb349a6a05e
Gabriel Martinez <jei.sum41@gmail.com>
2022-03-10 09:19:14 -0500

    jest - basic setup

---
+++ app/__tests__/components/payments/DeliveryWallet/DeliveryWallet.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react'
+import DeliveryWalletDetails, { DeliveryWalletDetailsProps } from '@components/payments/DeliveryWallet/DeliveryWalletDetails'
+describe('render payment components', () => {
+
+  Object.defineProperty(navigator, "clipboard", {
+    value: {
+      writeText: () => { console.debug('Copied!') },
+    },
+  })
+  let walletDetails: DeliveryWalletDetailsProps;
+  beforeAll(() => {
+    walletDetails = {
+      dictionary: {
+        purchaseInstructions: [],
+        walletInfo: [],
+        walletMultiSigTooltip: '',
+        wirePaymentsDisclaimer: []
+      },
+      walletAddress: '1231312312312',
+      wallets: []
+    }
+  })
+
+  it('render wallet details', async () => {
+    render(<DeliveryWalletDetails {...walletDetails} />)
+
+    const button = await screen.getByLabelText('Copy Wallet Address')
+    button.click()
+    expect(screen.getByDisplayValue(walletDetails.walletAddress)).toBeInTheDocument()
+    expect(screen.getByText('Copied', { exact: false })).toBeInTheDocument()
+  })
+})
