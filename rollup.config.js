8cc20065747941c4aefee97cd6c9fcf4e2b4c566
robdev91 <robdev91@gmail.com>
2022-05-27 10:46:44 -0400

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,12 @@
 import commonjs from "@rollup/plugin-commonjs";
+import dts from "rollup-plugin-dts"
 import resolve from "@rollup/plugin-node-resolve";
-import * as fs from 'fs';
-import analyze from 'rollup-plugin-analyzer';
-import dts from "rollup-plugin-dts";
-import typescript from "rollup-plugin-typescript2";
-import { visualizer } from 'rollup-plugin-visualizer';
-import pkg from "./package.json";
-
-
+import typescript from "rollup-plugin-typescript2"
 // import { terser } from "rollup-plugin-terser";
 // import { babel } from "@rollup/plugin-babel";
 // import externals from "rollup-plugin-node-externals";
 
-
-function writeTo(analysisString) {
-  try {
-    fs.writeFileSync('./stats.md', analysisString);
-  } catch(err) {
-    console.log('Could not write stats.md.', err);
-  }
-}
-
-const RUN_BUNDLE_ANALYSIS = !process.env.GITHUB_ACTIONS && true;
-
-if (RUN_BUNDLE_ANALYSIS) {
-  console.log('\n\nRunning build with bundle analysis...\n');
-}
-
-
-// =============================================================================
-//
-// BUNDLE SIZE ANALYSIS:
-//
-// The build will run in bundle analysis mode when running locally by default. If you want
-//
-// If you want to build a production bundle, please, change the `&& true` in `RUN_BUNDLE_ANALYSIS` to `false`.
-//
-// The build will always run in production mode when running on GitHub Actions.
-//
-// =============================================================================
-
+import pkg from "./package.json";
 
 // Extensions handled by babel:
 // const EXTENSIONS = [".ts", ".tsx"];
@@ -47,7 +14,7 @@ if (RUN_BUNDLE_ANALYSIS) {
 // Exclude dev dependencies:
 const EXTERNAL = [
   ...Object.keys(pkg.devDependencies),
-  ...(RUN_BUNDLE_ANALYSIS ? [] : Object.keys(pkg.dependencies)),
+  ...Object.keys(pkg.dependencies),
   ...Object.keys(pkg.peerDependencies),
 
   // See issues https://github.com/rollup/rollup/issues/3684, https://rollupjs.org/guide/en/#warning-treating-module-as-external-dependency:
@@ -115,10 +82,7 @@ export default [{
     // }),
 
     // terser(),
-
-    RUN_BUNDLE_ANALYSIS ? analyze({ writeTo }) : null,
-    RUN_BUNDLE_ANALYSIS ? visualizer() : null,
-  ].filter(Boolean),
+  ],
 
 }, {
   input: "./dist/types/index.d.ts",
